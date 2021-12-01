1abcf5540533647bd46231fcc0860807f9938dde
PitchapaSaelim <pitchapa.sae@ku.th>
2021-12-01 16:25:28 +0700

    Fixed some codes

--- controller.py
+++ controller.py
@@ -1,16 +1,18 @@
+from openapi_server import models
 import sys
 from flask import abort
 import pymysql as mysql
 from config import OPENAPI_AUTOGEN_DIR, DB_HOST, DB_USER, DB_PASSWD, DB_NAME
 
 sys.path.append(OPENAPI_AUTOGEN_DIR)
-from openapi_server import models
+
 
 def db_cursor():
     return mysql.connect(host=DB_HOST,
                          user=DB_USER,
                          passwd=DB_PASSWD,
                          db=DB_NAME).cursor()
+
 
 def get_speeds():
     with db_cursor() as cs:
@@ -20,6 +22,7 @@ def get_speeds():
         """)
         result = [models.Speed(*row) for row in cs.fetchall()]
         return result
+
 
 def get_specific_speed(speed_id):
     with db_cursor() as cs:
@@ -35,6 +38,7 @@ def get_specific_speed(speed_id):
     else:
         abort(404)
 
+
 def get_specific_pm25(speed_id):
     with db_cursor() as cs:
         cs.execute("""
@@ -47,6 +51,7 @@ def get_specific_pm25(speed_id):
         return result[0]
     else:
         abort(404)
+
 
 def get_specific_temperature(speed_id):
     with db_cursor() as cs:
@@ -61,6 +66,7 @@ def get_specific_temperature(speed_id):
     else:
         abort(404)
 
+
 def get_specific_humidity(speed_id):
     with db_cursor() as cs:
         cs.execute("""
@@ -74,6 +80,7 @@ def get_specific_humidity(speed_id):
     else:
         abort(404)
 
+
 def get_specific_rainfall(speed_id):
     with db_cursor() as cs:
         cs.execute("""
@@ -86,6 +93,7 @@ def get_specific_rainfall(speed_id):
         return result[0]
     else:
         abort(404)
+
 
 def get_traffic_report(lat, lon):
     with db_cursor() as cs:
@@ -101,12 +109,13 @@ def get_traffic_report(lat, lon):
         start = result[1].strftime("%Y/%m/%d %H:%M:%S").split()
         end = result[2].strftime("%Y/%m/%d %H:%M:%S").split()
         if result[3] >= 45:
-            report = "not a traffic jam"
+            report = "Clear road"
         else:
-            report = "traffic jam"
+            report = "Traffic jam"
         return models.TrafficReport(date=start[0], time=start[1]+" - "+end[1], traffic_status=report)
     else:
         abort(404)
+
 
 def get_air_report(lat, lon):
     with db_cursor() as cs:
@@ -122,14 +131,15 @@ def get_air_report(lat, lon):
         start = result[1].strftime("%Y/%m/%d %H:%M:%S").split()
         end = result[2].strftime("%Y/%m/%d %H:%M:%S").split()
         if result[3] >= 70:
-            report = "อากาศแย่"
+            report = "Unhealthy"
         elif result[3] >= 57:
-            report = "อากาศปานกลาง"
+            report = "Moderate"
         else:
-            report = "อากาศดี"
+            report = "Good"
         return models.AqiReport(date=start[0], time=start[1]+" - "+end[1], air_quality=report, pm25=result[3])
     else:
         abort(404)
+
 
 def get_tmd_report(lat, lon):
     with db_cursor() as cs:
@@ -145,110 +155,9 @@ def get_tmd_report(lat, lon):
         start = result[1].strftime("%Y/%m/%d %H:%M:%S").split()
         end = result[2].strftime("%Y/%m/%d %H:%M:%S").split()
         if result[5] > 0:
-            report = "ฝนตก"
+            report = "Rain"
         else:
-            report = "ฝนไม่ตก"
+            report = "Cleary sky"
         return models.TmdReport(date=start[0], time=start[1]+" - "+end[1], rain_status=report, rainfall=result[5])
     else:
         abort(404)
-
-# def get_basins():
-#     with db_cursor() as cs:
-#         cs.execute("""
-#             SELECT basin_id, ename, area
-#             FROM basin
-#         """)
-#         result = [models.Basin(*row) for row in cs.fetchall()]
-#         return result
-
-# def get_basin_details(basin_id):
-#     with db_cursor() as cs:
-#         cs.execute("""
-#             SELECT basin_id, ename, area
-#             FROM basin
-#             WHERE basin_id=%s
-#             """, [basin_id])
-#         result = cs.fetchone()
-#     if result:
-#         basin_id, ename, area = result
-#         return models.Basin(*result)
-#     else:
-#         abort(404)
-
-# def get_basin_geom(basin_id):
-#     with db_cursor() as cs:
-#         cs.execute("""
-#             SELECT ST_AsText(geometry)
-#             FROM basin
-#             WHERE basin_id=%s
-#             """, [basin_id])
-#         result = cs.fetchone()
-#     if result:
-#         return result[0]
-#     else:
-#         abort(404)
-
-# def get_stations_in_basin(basin_id):
-#     with db_cursor() as cs:
-#         cs.execute("""
-#             SELECT station_id, basin_id, ename, lat, lon
-#             FROM station WHERE basin_id=%s
-#             """, [basin_id])
-#         result = [models.Station(*row) for row in cs.fetchall()]
-#         return result
-
-# def get_station_details(station_id):
-#     with db_cursor() as cs:
-#         cs.execute("""
-#             SELECT station_id, basin_id, ename, lat, lon 
-#             FROM station 
-#             WHERE station_id=%s
-#             """, [station_id])
-#         result = cs.fetchone()
-#     if result:
-#         return models.Station(*result)
-#     else:
-#         abort(404)
-
-# def get_basin_annual_rainfall(basin_id, year):
-#     with db_cursor() as cs:
-#         cs.execute("""
-#             SELECT SUM(daily_avg)
-#             FROM (
-#                 SELECT r.year, r.month, r.day, AVG(r.amount) as daily_avg
-#                 FROM rainfall r
-#                 INNER JOIN station s ON r.station_id=s.station_id
-#                 INNER JOIN basin b ON b.basin_id=s.basin_id
-#                 WHERE b.basin_id=%s AND r.year=%s
-#                 GROUP BY r.year, r.month, r.day
-#             ) daily_avg
-#         """, [basin_id, year])
-#         result = cs.fetchone()
-#     if result and result[0]:
-#         amount = round(result[0], 2)
-#         return amount
-#     else:
-#         abort(404)
-
-# def get_basin_monthly_average(basin_id):
-#     with db_cursor() as cs:
-#         cs.execute("""
-#             SELECT month, AVG(monthly_amount)
-#             FROM (
-#                 SELECT SUM(r.amount) as monthly_amount, month
-#                 FROM rainfall r
-#                 INNER JOIN station s ON r.station_id = s.station_id
-#                 INNER JOIN basin b ON s.basin_id = b.basin_id
-#                 WHERE b.basin_id=%s
-#                 GROUP BY r.station_id, month, year
-#             ) monthly
-#             GROUP BY month
-#             ORDER BY month ASC
-#             """, [basin_id])
-#         months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-#                   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-#         result = [
-#                 models.MonthlyAverage(months[month-1], month, round(amount, 2))
-#                 for month, amount in cs.fetchall()
-#             ]
-#         return result
